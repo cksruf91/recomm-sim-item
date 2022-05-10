72ca289d1c9e664c48c10a0aef5bedad7aca9071
chan <cksruf91@gmail.com>
2022-05-10 04:50:59 +0000

    fix model

--- model/item2vec.py
+++ model/item2vec.py
@@ -6,26 +6,18 @@ class Item2Vector(torch.nn.Module):
     def __init__(self, nitem: int, emb_dim: int, device=torch.device('cpu')):
         super().__init__()
         self.embedding_1 = torch.nn.Embedding(nitem, emb_dim)
-        # self.embedding_2 = torch.nn.Embedding(nitem, emb_dim)
-        self.fc_1 = torch.nn.Linear(emb_dim*2, emb_dim)
-        self.activation = torch.nn.ReLU()
-        self.fc_2 = torch.nn.Linear(emb_dim, 1)
+        self.embedding_2 = torch.nn.Embedding(nitem, emb_dim)
+        self.sigmoid= torch.nn.Sigmoid()
+        self.to(device)
         
     def forward(self, center, context):
         emb_1 = self.embedding_1(center)
-        emb_2 = self.embedding_1(context)
-        
-        x = torch.concat([emb_1, emb_2], dim=-1)
-        x = self.fc_1(x)
-        x = self.activation(x)
-        x = self.fc_2(x)
-        
-        return x.squeeze()
+        emb_2 = self.embedding_2(context)
+        x = torch.sum(torch.mul(emb_1, emb_2), dim=-1)
+        return self.sigmoid(x)
     
     def get_vector(self):
         for name, param in self.embedding_1.named_parameters():
             with torch.no_grad():
                 param = param.cpu().numpy()
         return param
-        
-        
